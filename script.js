b229d45edd24e378778536737a263cabe7a430a0
arafiyasin <114900694+arafiyasin@users.noreply.github.com>
2023-08-29 00:39:52 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
+document.querySelectorAll('nav a').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        
+        const target = document.querySelector(this.getAttribute('href'));
+        target.scrollIntoView({
+            behavior: 'smooth',
+            block: 'start'
+        });
+    });
+});
+
 const slider = document.querySelector('.slider');
 const handle = document.querySelector('.handle');
 const sun = document.querySelector('.sun');
