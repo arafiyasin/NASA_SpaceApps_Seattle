01a508e5f917281a3dde49c9398af5488ab0a8ff
arafiyasin <114900694+arafiyasin@users.noreply.github.com>
2023-08-29 00:19:32 -0700

    Added primary files

---
+++ script.js
@@ -0,0 +1,38 @@
+const slider = document.querySelector('.slider');
+const handle = document.querySelector('.handle');
+const output = document.getElementById('output');
+
+let angle = 0; // Angle in degrees
+
+function updateSliderPosition() {
+    const radius = slider.clientWidth / 2;
+    const x = radius * Math.cos(angle * (Math.PI / 180));
+    const y = radius * Math.sin(angle * (Math.PI / 180));
+
+    handle.style.transform = `translate(${x}px, ${y}px)`;
+    output.textContent = `Angle: ${angle}°`;
+
+    // Update other information or trigger actions based on the angle
+    // For example, you can display the picture of the sun based on the angle.
+}
+
+function dragSlider(event) {
+    event.preventDefault();
+    
+    const sliderRect = slider.getBoundingClientRect();
+    const center = { x: sliderRect.left + sliderRect.width / 2, y: sliderRect.top + sliderRect.height / 2 };
+    
+    const dx = event.clientX - center.x;
+    const dy = event.clientY - center.y;
+    
+    angle = (Math.atan2(dy, dx) * 180 / Math.PI) + 90;
+    
+    updateSliderPosition();
+}
+
+handle.addEventListener('mousedown', () => {
+    document.addEventListener('mousemove', dragSlider);
+    document.addEventListener('mouseup', () => {
+        document.removeEventListener('mousemove', dragSlider);
+    });
+});
